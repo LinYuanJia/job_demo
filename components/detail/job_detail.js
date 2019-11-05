e802f3f8069a2181227b07a35ff570ecb599f608
yuanjia <yuanjia@sina.cn>
2019-11-05 20:53:26 +0800

    组件job_list更新-->接入后台数据

--- components/detail/job_detail.js
+++ components/detail/job_detail.js
@@ -4,20 +4,16 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    jobList: {
+      type:Object,
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    job_list:{
-        message: "苹果手机客服",
-        salary: "3500.00/月",
-        condition: "靖江区 | 男女不限 | 18岁~25岁",
-        fuli: ["五险一金", "交通补贴", "年终奖", "加班补贴"],
-        time: "2小时前发布"
-      }
+   
   },
 
   /**
